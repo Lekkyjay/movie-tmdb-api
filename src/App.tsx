d9bb122670b472855af9e33f6923da9f94b80c2d
‘Lekky <lekkyjay@yahoo.com>
2024-08-01 19:32:10 +0200

    Folder structure and layout initiated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import tmdbApi from './api/tmdbApi'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Catalog from './pages/Catalog'
+import Detail from './pages/detail/Detail'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState<any>([])
+
+  useEffect(() => {
+    const getList = async () => {
+      let response = null;
+      const params = {};
+      response = await tmdbApi.getMoviesList('popular', {params})
+      console.log(response)
+      setItems(response.results)
+    }
+    getList()
+  }, [])
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />      
+        },
+        {
+          path: "/:category",
+          element: <Catalog />      
+        },
+        {
+          path: "/:category/search/:keyword",
+          element: <Catalog />      
+        },
+        {
+          path: "/:category/:id",
+          element: <Detail />      
+        }
+      ]
+    }
+  ])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
   )
 }
